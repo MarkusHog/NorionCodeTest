1f53e4c51113a6caca673864787921909558ef07
MarkusHog <112397244+MarkusHog@users.noreply.github.com>
2024-04-07 01:28:47 +0200

    Added a very simple endpoint to test

--- NorionCodeTest/Program.cs
+++ NorionCodeTest/Program.cs
@@ -2,6 +2,7 @@ using Application;
 using Domain.Abstractions;
 using Domain.Models;
 using Domain.Utility;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,28 @@ builder.Services.AddScoped<IPassageCost, PassageCost>();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Added a simple API endpoint to test the toll calculation
+//Test with query parameters
+// https://localhost:7121/api/toll?vehicle=car&dates=2024-04-04T08:15:00&dates=2024-04-04T17:30:00
+app.MapGet("/api/toll", (ITollCalculator tollCalculator, string vehicle, DateTime[] dates) =>
+    {
+        if (!string.Equals(vehicle, "car", StringComparison.OrdinalIgnoreCase))
+        {
+            return Results.Ok(0);
+        }
+        if (dates is null)
+        {
+            return Results.BadRequest("Dates are required");
+        }
+        var car = new Car();
+        var type = car.GetVehicleType();
+        var result = tollCalculator.GetTollFee(car, dates);
+        return Results.Ok(result);
+    }       
+);
 
 app.Run();
+
+
+
+
